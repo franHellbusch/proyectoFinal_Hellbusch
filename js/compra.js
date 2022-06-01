32d48af07aa15253a30d3b74cef18d5ee599cc9b
franHellbusch <fghellbusch@gmail.com>
2022-06-01 00:10:00 -0300

    agregando fetxh

--- js/compra.js
+++ js/compra.js
@@ -1,45 +1,222 @@
 let estaLogeado =  sessionStorage.getItem("logeado?")
 let usuarioLogeado = JSON.parse(localStorage.getItem("usuarioLogeado"));
-// constructor
-class Producto{
-    constructor (imagen, nombre, desc, precio, id){
-        this.imagen = imagen
-        this.nombre = nombre
-        this.desc = desc
-        this.precio = precio
-        this.id = id
-        this.vendido = false
+
+// carrito
+let contenidoCarrito = [];
+
+// cards
+let cardDeck = document.getElementById("cardDeck");
+
+function filtros(datos) {
+    for (let propiedad in datos[num]){
+        for (i = 0;i <1; i++){
+            let cards = document.createElement("div");
+            cards.className = "col mb-4";
+            cards.innerHTML = `<div id="card" class="card${i + 1} border mt-3 rounded"><img id="img" src="../assets/img/${datos[num][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body text-center"><h5 class="card-title">${datos[num][propiedad].nombre}</h5><p class="card-text">${datos[num][propiedad].desc}</p><botton id="${datos[num][propiedad].id}" class="btn btn-outline-success">COMPRAR ($${datos[num][propiedad].precio})</bottom></div></div>`;
+            cardDeck.append(cards)
+        }
     }
 }
-let productos2 = []
+
+// funcion de Filtro: todos los productos
+function todosFiltro(datos) {
+    for (i = 0; i < 3; i++){
+        for (let propiedad in datos[i]){
+            for (index = 0;index <1; index++){
+                let cards = document.createElement("div");
+                cards.className = "col mb-4";
+                cards.innerHTML = `<div id="card" class="card${i + 1} border mt-3 rounded"><img id="img" src="../assets/img/${datos[i][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body text-center"><h5 class="card-title">${datos[i][propiedad].nombre}</h5><p class="card-text">${datos[i][propiedad].desc}</p><botton id="${datos[i][propiedad].id}" class="btn btn-outline-success">COMPRAR ($${datos[i][propiedad].precio})</botton></div></div>`;
+                cardDeck.append(cards)
+            }
+        }
+    }
+}
+
+// seccion de Carrito de compra
+let carrito = document.getElementById("carritoContenido");
+let vaciarCarrito = document.getElementById("borrarCarrito");
+let totalCarrito = document.getElementById("totalCarrito");
+let remera1
+let remera2
+let remera3
+let pantalon1
+let pantalon2
+let pantalon3
+let buzo1
+let buzo2
+
+vaciarCarrito.onclick = () => {
+    while (carrito.firstChild) {
+        carrito.removeChild(carrito.firstChild);
+    }
+    contenidoCarrito = []
+    sacarTotal();
+    badge()
+}
+
+function sacarTotal() {
+    let total = contenidoCarrito.reduce ((acc,el) => acc + el.precio, 0);
+    totalCarrito.innerHTML = `TOTAL: ${total}`;
+}
+
+function borrarProducto(nombre) {
+    let position = contenidoCarrito.indexOf(nombre)
+    contenidoCarrito.splice(position, 1);
+    carrito.removeChild(carrito.childNodes[position])   
+    sacarTotal();
+    badge();
+}
+
+function comprarRemeras(datos) {
+    let boton1 = document.getElementById("remeraCorta");
+    let boton2 = document.getElementById("remeraLarga");
+    let boton3 = document.getElementById("remeraMusculosa");
+    boton1.onclick = () => {
+        let remeraCorta = document.createElement("li");
+        remeraCorta.className = "list-group-item ml-3 bg-transparent"
+        remeraCorta.innerHTML = `Remera de mangas cortas ($2500) <buttom class="btn btn-danger" onclick="borrarProducto(remera1)">x</buttom>`;
+        carrito.append(remeraCorta);
+        remera1 = datos[0].remera1;
+        contenidoCarrito.push(remera1);
+        sacarTotal();
+        badge();
+    }
+    boton2.onclick = () => {
+        let remeraLarga = document.createElement("li");
+        remeraLarga.className = "list-group-item ml-3 bg-transparent";
+        remeraLarga.innerHTML = `Remera de mangas largas ($1900) <buttom class="btn btn-danger" onclick="borrarProducto(remera2)">x</buttom>`;
+        carrito.append(remeraLarga);
+        remera2 = datos[0].remera2;
+        contenidoCarrito.push(remera2);
+        sacarTotal();
+        badge();
+    }
+    boton3.onclick = () => {
+        let remeraMusculosa = document.createElement("li");
+        remeraMusculosa.className = "list-group-item ml-3 bg-transparent";
+        remeraMusculosa.innerHTML = `Remera de musculosa ($2100) <buttom class="btn btn-danger" onclick="borrarProducto(remera3)">x</buttom>`;
+        carrito.append(remeraMusculosa);
+        remera3 = datos[0].remera3;
+        contenidoCarrito.push(remera3);
+        sacarTotal();
+        badge();
+    }
+}
+
+function comprarPantalones(datos) {
+    let boton1 = document.getElementById("pantalonCorto");
+    let boton2 = document.getElementById("pantalonJean");
+    let boton3 = document.getElementById("pantalonJogging");
+    boton1.onclick = () => {
+        let pantalonCorto = document.createElement("li");
+        pantalonCorto.className = "list-group-item ml-3 bg-transparent";
+        pantalonCorto.innerHTML = `Pantalon corto ($2700) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon1)">x</buttom>`;
+        carrito.append(pantalonCorto);
+        pantalon1 = datos[1].pantalon1;
+        contenidoCarrito.push(pantalon1);
+        sacarTotal();
+        badge();
+    }
+    boton2.onclick = () => {
+        let pantalonJean = document.createElement("li");
+        pantalonJean.className = "list-group-item ml-3 bg-transparent";
+        pantalonJean.innerHTML = `Pantalon jean ($3000) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon2)">x</buttom>`;
+        carrito.append(pantalonJean);
+        pantalon2 = datos[1].pantalon2;
+        contenidoCarrito.push(pantalon2);
+        sacarTotal();
+        badge();
+    }
+    boton3.onclick = () => {
+        let pantalonJogging = document.createElement("li");
+        pantalonJogging.className = "list-group-item ml-3 bg-transparent";
+        pantalonJogging.innerHTML = `Pantalon jogging ($2800) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon3)">x</buttom>`;
+        carrito.append(pantalonJogging);
+        pantalon3 = datos[1].pantalon3;
+        contenidoCarrito.push(pantalon3);
+        sacarTotal();
+        badge();
+    }
+}
+
+function comprarBuzos(datos) {
+    let boton1 = document.getElementById("buzoCapucha");
+    let boton2 = document.getElementById("buzoSinCapucha");
+    boton1.onclick = () => {
+        let buzoCapucha = document.createElement("li");
+        buzoCapucha.className = "list-group-item ml-3 bg-transparent";
+        buzoCapucha.innerHTML = `Buzo con capucha ($4000) <buttom class="btn btn-danger" onclick="borrarProducto(buzo1)">x</buttom>`;
+        carrito.append(buzoCapucha);
+        buzo1 = datos[2].buzo1;
+        contenidoCarrito.push(buzo1);
+        sacarTotal();
+        badge();
+    }
+    boton2.onclick = () => {
+        let buzoSinCapucha = document.createElement("li");
+        buzoSinCapucha.className = "list-group-item ml-3 bg-transparent";
+        buzoSinCapucha.innerHTML = `Buzo sin capucha ($3500) <buttom class="btn btn-danger" onclick="borrarProducto(buzo2)">x</buttom>`;
+        carrito.append(buzoSinCapucha);
+        buzo2 = datos[2].buzo2;
+        contenidoCarrito.push(buzo2);
+        sacarTotal();
+        badge();
+    }
+}
+
 fetch(`../js/productos.json`)
 .then((response) => response.json())
 .then((data) => {
-    productos2.push(data[0]);
-    productos2.push(data[1]);
-    productos2.push(data[2]);
-    console.log(data)
-    console.log(productos2[0]);
-});
-// remeras
-let remera1 = new Producto ("remera-mangas-cortas.jpg", "Remera mangas cortas", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 2500, "remeraCorta");
-let remera2 = new Producto ("remera-mangas-largas.jpg", "Remera mangas largas", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 1900, "remeraLarga");
-let remera3 = new Producto ("remera-musculosa.jpg", "Remera musculosa", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 2100, "remeraMusculosa");
-// pantalones
-let pantalon1 = new Producto ("pantalon-corto.jpg", "Pantalon corto", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 2700, "pantalonCorto");
-let pantalon2 = new Producto ("pantalon-jean.webp", "Pantalon jean", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 3000, "pantalonJean");
-let pantalon3 = new Producto ("pantalon-jogging.webp", "Pantalon jogging", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 2800, "pantalonJogging");
-// buzo
-let buzo1 = new Producto ("buzo-capucha.jpg", "Buzo con capucha", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 4000, "buzoCapucha");
-let buzo2 = new Producto ("buzo-sin-capucha.png", "Buzo sin capucha", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", 3500, "buzoSinCapucha");
-// secciones
-const remeras = {remera1, remera2, remera3};
-const pantalones = {pantalon1, pantalon2, pantalon3};
-const buzos = {buzo1, buzo2};
-// todos los productos
-const productos = [remeras, pantalones, buzos];
-// carrito
-let contenidoCarrito = [];
+    console.log(data[1].pantalon1);
+    todosFiltro(data);
+    comprarRemeras(data);
+    comprarPantalones(data);
+    comprarBuzos(data);
+
+    let botonRemeras = document.getElementById("remeras");
+    let botonPantalones = document.getElementById("pantalones");
+    let botonBuzos = document.getElementById("buzos");
+    let botonTodos = document.getElementById("todos");
+
+
+    botonRemeras.onclick = () => {
+        while (cardDeck.firstChild) {
+            cardDeck.removeChild(cardDeck.firstChild);
+        }
+        num = 0;
+        filtros(data);
+        comprarRemeras(data);
+    }   
+
+    botonPantalones.onclick = () => {
+        while (cardDeck.firstChild) {
+            cardDeck.removeChild(cardDeck.firstChild);
+        }
+        num = 1;
+        filtros(data)
+        comprarPantalones(data)
+    }
+
+    botonBuzos.onclick = () => {
+        while (cardDeck.firstChild) {
+            cardDeck.removeChild(cardDeck.firstChild);
+        }
+        num = 2;
+        filtros(data)
+        comprarBuzos(data)
+    }
+
+    botonTodos.onclick = () => {
+        while (cardDeck.firstChild) {
+            cardDeck.removeChild(cardDeck.firstChild);
+        }
+        todosFiltro(data);
+        comprarBuzos(data);
+        comprarRemeras(data);
+        comprarPantalones(data);
+    }
+})
+.catch((error) => console.error(`error`, error));
 
 // side nav
 let iconCarrito = document.getElementById("iconCarrito");
@@ -76,202 +253,6 @@ controlFiltros2.onclick = () => {
     filtrosContenido.style.height = "0";
     controlFiltros1.style.display = "block";
     controlFiltros2.style.display = "none";
-}
-
-// cards
-let cardDeck = document.getElementById("cardDeck");
-
-function filtros() {
-    for (let propiedad in productos[num]){
-        for (i = 0;i <1; i++){
-            let cards = document.createElement("div");
-            cards.className = "col mb-4";
-            cards.innerHTML = `<div id="card" class="card${i + 1} border mt-3 rounded"><img id="img" src="../assets/img/${productos[num][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body text-center"><h5 class="card-title">${productos[num][propiedad].nombre}</h5><p class="card-text">${productos[num][propiedad].desc}</p><botton id="${productos[num][propiedad].id}" class="btn btn-outline-success">COMPRAR ($${productos[num][propiedad].precio})</bottom></div></div>`;
-            cardDeck.append(cards)
-        }
-    }
-}
-
-let num = 0
-todosFiltro();
-comprarRemeras();
-comprarPantalones();
-comprarBuzos();
-let botonRemeras = document.getElementById("remeras");
-let botonPantalones = document.getElementById("pantalones");
-let botonBuzos = document.getElementById("buzos");
-let botonTodos = document.getElementById("todos");
-
-
-botonRemeras.onclick = () => {
-    while (cardDeck.firstChild) {
-        cardDeck.removeChild(cardDeck.firstChild);
-    }
-    num = 0;
-    filtros();
-    comprarRemeras();
-}
-
-botonPantalones.onclick = () => {
-    while (cardDeck.firstChild) {
-        cardDeck.removeChild(cardDeck.firstChild);
-    }
-    num = 1;
-    filtros()
-    comprarPantalones()
-}
-
-botonBuzos.onclick = () => {
-    while (cardDeck.firstChild) {
-        cardDeck.removeChild(cardDeck.firstChild);
-    }
-    num = 2;
-    filtros()
-    comprarBuzos()
-}
-
-// funcion de Filtro: todos los productos
-function todosFiltro() {
-    for (i = 0; i < 3; i++){
-        for (let propiedad in productos[i]){
-            for (index = 0;index <1; index++){
-                let cards = document.createElement("div");
-                cards.className = "col mb-4";
-                cards.innerHTML = `<div id="card" class="card${i + 1} border mt-3 rounded"><img id="img" src="../assets/img/${productos[i][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body text-center"><h5 class="card-title">${productos[i][propiedad].nombre}</h5><p class="card-text">${productos[i][propiedad].desc}</p><botton id="${productos[i][propiedad].id}" class="btn btn-outline-success">COMPRAR ($${productos[i][propiedad].precio})</botton></div></div>`;
-                cardDeck.append(cards)
-            }
-        }
-    }
-}
-
-botonTodos.onclick = () => {
-    while (cardDeck.firstChild) {
-        cardDeck.removeChild(cardDeck.firstChild);
-    }
-    todosFiltro();
-    comprarBuzos();
-    comprarRemeras();
-    comprarPantalones();
-}
-
-// seccion de Carrito de compra
-let notificacionCompra = document.createElement("h3");
-notificacionCompra.className = "bg-success border position-absolute";
-notificacionCompra.innerHTML = "El producto fue enviado al carrito!"
-
-let carrito = document.getElementById("carritoContenido");
-let vaciarCarrito = document.getElementById("borrarCarrito");
-
-vaciarCarrito.onclick = () => {
-    while (carrito.firstChild) {
-        carrito.removeChild(carrito.firstChild);
-    }
-    contenidoCarrito = []
-    sacarTotal();
-    badge()
-}
-
-let totalCarrito = document.getElementById("totalCarrito");
-function sacarTotal() {
-    let total = contenidoCarrito.reduce ((acc,el) => acc + el.precio, 0);
-    totalCarrito.innerHTML = `TOTAL: ${total}`;
-}
-
-function borrarProducto(nombre) {
-    let position = contenidoCarrito.indexOf(nombre)
-    contenidoCarrito.splice(position, 1);
-    carrito.removeChild(carrito.childNodes[position])   
-    sacarTotal();
-    badge();
-}
-
-function comprarRemeras() {
-    let boton1 = document.getElementById("remeraCorta");
-    let boton2 = document.getElementById("remeraLarga");
-    let boton3 = document.getElementById("remeraMusculosa");
-    boton1.onclick = () => {
-        let remeraCorta = document.createElement("li");
-        remeraCorta.className = "list-group-item ml-3 bg-transparent"
-        remeraCorta.innerHTML = `Remera de mangas cortas ($2500) <buttom class="btn btn-danger" onclick="borrarProducto(remera1)">x</buttom>`;
-        carrito.append(remeraCorta);
-        contenidoCarrito.push(productos[0].remera1);
-        sacarTotal();
-        badge();
-    }
-    boton2.onclick = () => {
-        let remeraLarga = document.createElement("li");
-        remeraLarga.className = "list-group-item ml-3 bg-transparent";
-        remeraLarga.innerHTML = `Remera de mangas largas ($1900) <buttom class="btn btn-danger" onclick="borrarProducto(remera2)">x</buttom>`;
-        carrito.append(remeraLarga);
-        contenidoCarrito.push(productos[0].remera2);
-        sacarTotal();
-        badge();
-    }
-    boton3.onclick = () => {
-        let remeraMusculosa = document.createElement("li");
-        remeraMusculosa.className = "list-group-item ml-3 bg-transparent";
-        remeraMusculosa.innerHTML = `Remera de musculosa ($2100) <buttom class="btn btn-danger" onclick="borrarProducto(remera3)">x</buttom>`;
-        carrito.append(remeraMusculosa);
-        contenidoCarrito.push(productos[0].remera3);
-        sacarTotal();
-        badge();
-    }
-}
-
-function comprarPantalones() {
-    let boton1 = document.getElementById("pantalonCorto");
-    let boton2 = document.getElementById("pantalonJean");
-    let boton3 = document.getElementById("pantalonJogging");
-    boton1.onclick = () => {
-        let pantalonCorto = document.createElement("li");
-        pantalonCorto.className = "list-group-item ml-3 bg-transparent";
-        pantalonCorto.innerHTML = `Pantalon corto ($2700) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon1)">x</buttom>`;
-        carrito.append(pantalonCorto);
-        contenidoCarrito.push(productos[1].pantalon1);
-        sacarTotal();
-        badge();
-    }
-    boton2.onclick = () => {
-        let pantalonJean = document.createElement("li");
-        pantalonJean.className = "list-group-item ml-3 bg-transparent";
-        pantalonJean.innerHTML = `Pantalon jean ($3000) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon2)">x</buttom>`;
-        carrito.append(pantalonJean);
-        contenidoCarrito.push(productos[1].pantalon2);
-        sacarTotal();
-        badge();
-    }
-    boton3.onclick = () => {
-        let pantalonJogging = document.createElement("li");
-        pantalonJogging.className = "list-group-item ml-3 bg-transparent";
-        pantalonJogging.innerHTML = `Pantalon jogging ($2800) <buttom class="btn btn-danger" onclick="borrarProducto(pantalon3)">x</buttom>`;
-        carrito.append(pantalonJogging);
-        contenidoCarrito.push(productos[1].pantalon3);
-        sacarTotal();
-        badge();
-    }
-}
-
-function comprarBuzos() {
-    let boton1 = document.getElementById("buzoCapucha");
-    let boton2 = document.getElementById("buzoSinCapucha");
-    boton1.onclick = () => {
-        let buzoCapucha = document.createElement("li");
-        buzoCapucha.className = "list-group-item ml-3 bg-transparent";
-        buzoCapucha.innerHTML = `Buzo con capucha ($4000) <buttom class="btn btn-danger" onclick="borrarProducto(buzo1)">x</buttom>`;
-        carrito.append(buzoCapucha);
-        contenidoCarrito.push(productos[2].buzo1);
-        sacarTotal();
-        badge();
-    }
-    boton2.onclick = () => {
-        let buzoSinCapucha = document.createElement("li");
-        buzoSinCapucha.className = "list-group-item ml-3 bg-transparent";
-        buzoSinCapucha.innerHTML = `Buzo sin capucha ($3500) <buttom class="btn btn-danger" onclick="borrarProducto(buzo2)">x</buttom>`;
-        carrito.append(buzoSinCapucha);
-        contenidoCarrito.push(productos[2].buzo2);
-        sacarTotal();
-        badge();
-    }
 }
 
 // compra realizada
